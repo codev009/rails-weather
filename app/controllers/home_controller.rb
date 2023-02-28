00aba46d19a1067491c7912b0178515f274835c5
codev009 <lipin007@gmail.com>
2023-02-28 15:59:11 +0530

    refactor home template

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@
 
 class HomeController < ApplicationController
   def index
-    @data = CurrentWeatherService.new(latitude:"33.7490", longitude:"-84.3880", units:"imperial").call
+    data = CurrentWeatherService.new(latitude:"33.7490", longitude:"-84.3880", units:"imperial").call
+    @weather = Weather.new(data)
   end
 end
